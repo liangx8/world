e2e67d55cb8066fa69586047120648eac4d38841
no body <my-laptop@computer.com>
2019-06-26 17:57:36 +0800

    search the latest version on github automatically

--- get-v2ray.sh
+++ get-v2ray.sh
@@ -1,2 +1,4 @@
-VERSION="v4.7.0"
+VER_TAG_URL="https://api.github.com/repos/v2ray/v2ray-core/releases/latest"
+VERSION=`curl -s ${VER_TAG_URL} --connect-timeout 10 | grep 'tag_name' | cut -d\" -f4`
+echo "Downloading version ${VERSION}"
 curl -L -H "Cache-Control: no-cache" -O https://github.com/v2ray/v2ray-core/releases/download/${VERSION}/v2ray-linux-64.zip
