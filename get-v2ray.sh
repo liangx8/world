4b892e69969adfef04262b41c594eee2f55b26f1
Mr. Smith <im@home.com>
2022-11-12 16:07:03 +0800

    ...

--- get-v2ray.sh
+++ get-v2ray.sh
@@ -1,4 +1,6 @@
-VER_TAG_URL="https://api.github.com/repos/v2ray/v2ray-core/releases/latest"
+HOST_URL="https://api.github.com/repos/v2fly/v2ray-core/releases"
+VER_TAG_URL="${HOST_URL}/latest"
 VERSION=`curl -s ${VER_TAG_URL} --connect-timeout 10 | grep 'tag_name' | cut -d\" -f4`
 echo "Downloading version ${VERSION}"
-curl -L -H "Cache-Control: no-cache" -O https://github.com/v2ray/v2ray-core/releases/download/${VERSION}/v2ray-linux-64.zip
+echo $HOST_URL/download/${VERSION}/v2ray-linux-64.zip
+curl -L -H "Cache-Control: no-cache" -O $HOST_URL/download/${VERSION}/v2ray-linux-64.zip
